3015cdb8facfcb314d7a8348b05e0979ba980ec4
Zac Childress (oz_) <91855780+OZsoft-zac@users.noreply.github.com>
2021-10-19 22:08:55 -0500

    A small program to help me figure out a game card
    This program helped me figure out a digit that was accidentally scratched off a Steam gift card.

---
+++ gameCardInfo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+#include "Burrito.h"
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    char arr[36] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+    for (int i = 0; i < 36; i++) {
+        for (int j = 0; j < 36; j++) {
+            cout << "CX" << arr[i] << arr[j] << "J - LXAQ8 - JC5FK" << endl;
+        }
+    }
+
+    return 0;
+}
+
