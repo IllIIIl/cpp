dfe5aa73e7c15578a083e85ff747fc887319a2e0
Zac Childress (oz_) <91855780+OZsoft-zac@users.noreply.github.com>
2021-10-28 21:17:23 -0500

    Messing around with C++14/17 stuff

---
+++ printing/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    cout << "*\n**\n***\n****\n*****\n";
+
+    int n;
+
+    //Sum of 1 to num ints;
+    int sumOnetoN = (n + 1) * (n / 2);
+
+    cout<<"Enter your lucky nunmber\n";
+    cin>>num;
+    cout<<num;
+
+    cout << INT_MAX << endl;
+    cout << INT_MIN << endl;
+
+    const double GRAVITY {9.8};
+    cout << GRAVITY << endl;
+
+    int a {1};
+    int b {2};
+
+    int temp {a};
+    int a{b};
+    int b{temp};
+
+    return 0;
+}
